7c2bce51ec0ec11a8817f4c147c26e6c9f692cb3
Skibin Alexander <skibinalexander@gmail.com>
2023-08-30 00:34:10 +0300

    Add blockchain type for methods

--- Sources/CoreProviderApi/Repositories/CoreProviderTransactionRepository.swift
+++ Sources/CoreProviderApi/Repositories/CoreProviderTransactionRepository.swift
@@ -11,12 +11,14 @@ import BlockchainSdk
 public protocol CoreProviderTranactionRepository {
     /// Obtain estimated fee transaction
     func estimatedFee<T: Codable>(
+        blockchain: BlockchainSdk.Blockchain,
         transaction: T,
         completion: @escaping (Result<Provider_Dto.Transaction.Fee.Res, Error>) -> Void
     )
     
     /// Perform send transaction into blockchain
     func sendTransaction<T: Codable>(
+        blockchain: BlockchainSdk.Blockchain,
         transaction: T,
         completion: @escaping (Result<Provider_Dto.Transaction.Send.Res, Error>) -> Void
     )
