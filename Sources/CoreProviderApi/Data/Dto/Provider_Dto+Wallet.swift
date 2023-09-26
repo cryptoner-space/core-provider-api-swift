58f1fd977234141c0f92dd9d87893c7cfd6e20b4
Skibin Alexander <skibinalexander@gmail.com>
2023-09-27 00:04:00 +0300

    Add any parameters for request wallet information

--- Sources/CoreProviderApi/Data/Dto/Provider_Dto+Wallet.swift
+++ Sources/CoreProviderApi/Data/Dto/Provider_Dto+Wallet.swift
@@ -9,11 +9,13 @@ import Foundation
 import BlockchainSdk
 
 public extension Provider_Dto.Info.Wallet {
-    struct Req: Codable {
+    struct Req<P: Codable>: Codable {
         public let currency: Currency
+        public let data: P?
         
-        public init(currency: Currency) {
+        public init(currency: Currency, data: P? = nil) {
             self.currency = currency
+            self.data = data
         }
     }
     
@@ -21,7 +23,7 @@ public extension Provider_Dto.Info.Wallet {
         public let amounts: [AmountCurrency]
         public let data: P?
         
-        public init(amounts: [AmountCurrency], data: P?) {
+        public init(amounts: [AmountCurrency], data: P? = nil) {
             self.amounts = amounts
             self.data = data
         }
